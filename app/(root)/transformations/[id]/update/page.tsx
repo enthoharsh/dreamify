48e753fd51cd48353847fe8814df3ebba8fbe3c2
Harsh Enthusiastic <harshenthusiastic@Harshs-Laptop.local>
2024-03-11 09:43:42 +0530

    final commit

--- app/(root)/transformations/[id]/update/page.tsx
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -3,8 +3,8 @@ import { redirect } from "next/navigation";
 
 import Header from "@/components/shared/Header";
 import TransformationForm from "@/components/shared/TransformationForm";
-import { transformationTypes } from "@/constants";
-import { getUserById } from "@/lib/actions/user.actions";
+import { transformationTypes } from "@/constant";
+import { getUserById } from "@/lib/actions/user.action";
 import { getImageById } from "@/lib/actions/image.actions";
 
 const Page = async ({ params: { id } }: SearchParamProps) => {
